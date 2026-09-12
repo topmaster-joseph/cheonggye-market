1121e3d201b1a65ac0a3c0e4ce68c5a122d02b0f
topmaster-joseph <topmaster.joseph@gmail.com>
2026-09-12 09:23:30 +0900

    chore: bust membership map style cache

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -16,7 +16,7 @@ function shouldEnhanceMerchantDirectory(pathname) {
 
 class MerchantDirectoryHead {
   element(element) {
-    element.append('<script src="/merchant-directory-web.js?v=20260912-regular-associate-v2" defer></script>', { html: true });
+    element.append('<script src="/merchant-directory-web.js?v=20260912-regular-54-map-v3" defer></script>', { html: true });
   }
 }
 
